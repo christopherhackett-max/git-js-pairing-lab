7455a4339e6c4e3844558562b08e6b5355656c24
Christopher Hackett <Christopherhackett20@gmail.com>
2025-10-01 15:37:03 -0400

    third function

--- index.js
+++ index.js
@@ -14,4 +14,15 @@ const multiplesOfThree = () => {
     }
 
 }
-multiplesOfThree()
+//multiplesOfThree()
+
+
+const multiplesOfThreeOrFive = () => {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            console.log(i)
+        }
+    }
+
+}
+//multiplesOfThreeOrFive()
