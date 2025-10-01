33e55de16ec64e4e16722b8db0675f87b009ec29
Evelin Cobos <evecobos8@gmail.com>
2025-10-01 15:57:47 -0400

    sixth function

--- index.js
+++ index.js
@@ -41,3 +41,17 @@ const multiply = (num1, num2) => {
     return num1 * num2
 }
 //console.log(multiply(4, 7))
+
+
+
+
+const add = (num1, num2) => {
+    let sum = num1 + num2
+
+    if (num1 === num2) {
+        return sum * 3
+    } else {
+        return sum
+    }
+}
+//console.log(add(2, 3))
